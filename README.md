e5bbb2effa458a9acf6ecfd26b21babd169ed765
ashrajesh <ashwinr@smu.edu>
2022-02-09 11:24:00 -0600

    core requirements satisfied

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Compile:
 Run:
 
 `C:\...\cs7319hw1> java pipenfilter`
+
+
+### Data Structure Selection
+ Reason for choosing Set over Vector or Map...
+ Set only stores unique values and has great lookup time.
+ Vector would require too much effort in searching for duplicates before inserting.
--- pipenfilter.java
+++ pipenfilter.java
@@ -1,10 +1,5 @@
-
-// identify delimiters (' ', ',', '.', '!', '?')
-// convert to lowercase
-// find on datastruct
-// if unique, then push to struct
-// print out unique words
 import java.io.*;
+import java.util.*;
 
 class pipenfilter {
     public static String readFileAsString(String fileName) throws Exception {
@@ -23,8 +18,29 @@ class pipenfilter {
         return everything;
     }
 
+    public static String cleanData(String data) {
+        String dataOne = data.replaceAll("[!,.?\n]", "").toLowerCase();
+        return dataOne;
+    }
+
+    public static void parseWords(String data, Set<String> words) {
+        String[] str = data.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            words.add(str[i]);
+        }
+    }
+
+    public static void printUniqueWords(Set<String> words) {
+        for (String j : words) {
+            System.out.println(j);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
+        Set<String> words = new HashSet<String>();
         String data = readFileAsString("pftest.txt");
-        System.out.println(data);
+        data = cleanData(data);
+        parseWords(data, words);
+        printUniqueWords(words);
     }
 }
