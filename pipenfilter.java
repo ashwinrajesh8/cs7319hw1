854e467f48e7e1848edd2e44b379c3d3f722a7fa
ashrajesh <ashwinr@smu.edu>
2022-02-16 10:27:06 -0600

    core requirements met
    - sorted output alphabetically
    - was advised instead of batch sequential to implement threading via the run interface

--- pipenfilter.java
+++ pipenfilter.java
@@ -31,18 +31,38 @@ class pipenfilter {
     }
 
     public static void printUniqueWords(Set<String> words) {
-        for (String j : words) {
+        List<String> orderedWords = new ArrayList<String>(words);
+        Collections.sort(orderedWords);
+        for (String j : orderedWords) {
             System.out.println(j);
         }
     }
 
+    private static class PipeRunner
+            implements Runnable {
+        public void run() {
+            String data = ""; // Initialize data, this will be our first variable passed through pipe and
+                              // filter
+            try {
+                Set<String> words = new HashSet<String>(); // Initialize our collection of unique words as an
+                                                           // unpopulated hashset
+                try {
+                    data = readFileAsString("pftest.txt"); // Read input file and pass it into data variable
+                } catch (Exception e) {
+                    System.out.println("Problem reading input.");
+                }
+                String dataClean = cleanData(data); // Filter data to meet criterea and pass it into dataClean variable
+                parseWords(dataClean, words); // Pass data from dataClean string into words hashset
+                printUniqueWords(words); // Output unique words found in input (by first filtering the words hashset and
+                                         // rearranging into alphabetically sorted array)
+            } catch (Exception e) {
+                System.out.println("Error upon running pipe and filter program.");
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        Set<String> words = new HashSet<String>();
-        String data = readFileAsString("pftest.txt");
-        String dataClean = cleanData(data);
-        parseWords(dataClean, words);
-        printUniqueWords(words);
-        // instead of batch sequential
-        // implenet multithreading the run interface
+        Thread t = new Thread(new PipeRunner());
+        t.start();
     }
 }
