ec8184eaa19799c9d06e5aa237926b401fd50399
ashrajesh <ashwinr@smu.edu>
2022-02-17 18:54:21 -0600

    work in progress...

--- pipenfilter.java
+++ pipenfilter.java
@@ -2,6 +2,12 @@ import java.io.*;
 import java.util.*;
 
 class pipenfilter {
+    /* Initialize variables that will be passed through "pipes" */
+    String data = "";
+    String dataClean = "";
+    Set<String> words = new HashSet<String>(); // Initialize our collection of unique words as an
+                                               // unpopulated hashset
+
     public static String readFileAsString(String fileName) throws Exception {
         String everything;
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
@@ -40,29 +46,79 @@ class pipenfilter {
 
     private static class PipeRunner
             implements Runnable {
-        public void run() {
-            String data = ""; // Initialize data, this will be our first variable passed through pipe and
-                              // filter
+        public PipeRunner(String data) {
             try {
-                Set<String> words = new HashSet<String>(); // Initialize our collection of unique words as an
-                                                           // unpopulated hashset
-                try {
-                    data = readFileAsString("pftest.txt"); // Read input file and pass it into data variable
-                } catch (Exception e) {
-                    System.out.println("Problem reading input.");
-                }
-                String dataClean = cleanData(data); // Filter data to meet criterea and pass it into dataClean variable
+                data = readFileAsString("pftest.txt"); // Read input file and pass it into data variable
+            } catch (Exception e) {
+                System.out.println("Problem reading input.");
+            }
+        }
+
+        public void run() {
+
+        }
+    }
+
+    private static class PipeRunnerTwo
+            implements Runnable {
+        public PipeRunnerTwo(String dataClean, String data) {
+            try {
+                dataClean = cleanData(data); // Filter data to meet criterea and pass it into dataClean variable
+            } catch (Exception e) {
+                System.out.println("Error filtering and cleaning raw data.\n");
+            }
+        }
+
+        public void run() {
+
+        }
+    }
+
+    private static class PipeRunnerThree
+            implements Runnable {
+        public PipeRunnerThree(String dataClean, Set<String> words) {
+            try {
                 parseWords(dataClean, words); // Pass data from dataClean string into words hashset
+            } catch (Exception e) {
+                System.out.println("Error passing data into hashset.\n");
+            }
+        }
+
+        public void run() {
+
+        }
+    }
+
+    private static class PipeRunnerFour
+            implements Runnable {
+        public PipeRunnerFour(Set<String> words) {
+            try {
                 printUniqueWords(words); // Output unique words found in input (by first filtering the words hashset and
                                          // rearranging into alphabetically sorted array)
             } catch (Exception e) {
-                System.out.println("Error upon running pipe and filter program.");
+                System.out.println("Error upon sorting & outputting filtered dataset.\n");
             }
+        }
+
+        public void run() {
+
         }
     }
 
     public static void main(String[] args) throws Exception {
-        Thread t = new Thread(new PipeRunner());
+        /* Run threads of components */
+        Thread t = new Thread(new PipeRunner(data));
         t.start();
+        Thread u = new Thread(new PipeRunnerTwo(dataClean, data));
+        u.start();
+        Thread v = new Thread(new PipeRunnerThree(dataClean, words));
+        v.start();
+        Thread w = new Thread(new PipeRunnerFour(words));
+        w.start();
+
+        t.join();
+        u.join();
+        v.join();
+        w.join();
     }
 }
